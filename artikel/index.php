9074ebf19fdaacc4c2636f66037156d32ea5136d
riskinurhadi <rizkibinmangtrisno@gmail.com>
2025-12-10 15:34:25 +0700

    modified:   artikel/index.php 	modified:   index.php

--- artikel/index.php
+++ artikel/index.php
@@ -134,7 +134,7 @@ function formatDate($date) {
         <div class="container">
             <a class="navbar-brand" href="index.php">
                 <i class=""></i>
-                <span>Dibikinin.Artikel</span>
+                <span>Dibikinin.Media</span>
             </a>
             
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
